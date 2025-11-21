cd48b75a93d2ff66e1772b03de453c652b669f1b
Kevin Middleton <middletonk@missouri.edu>
2025-11-21 15:53:22 -0600

    Add requirements and update batch render

--- batch_render.R
+++ batch_render.R
@@ -1,9 +1,82 @@
 # Lecture slides
-options(warn=2)
+options(warn = 2)
 
-qmds <- list.files(pattern = "^[0-9]*-[1-6].*qmd$",
-                   path = "Lectures/",
-                   full.names = TRUE)
+install_all_packages <- TRUE
+
+if (install_all_packages) {
+  if (!require("pak", quietly = TRUE)) {
+    install.packages("pak")
+  }
+
+  pak::pak(c(
+    "tidyverse",
+    "readxl",
+    "broom",
+    "cowplot",
+    "viridis",
+    "wesanderson",
+    "ggsci",
+    "latex2exp",
+    "ggtext",
+    "ggrepel",
+    "ggforce",
+    "ggridges",
+    "ggflowchart",
+    "ggpubr",
+    "plotly",
+    "GGally",
+    "patchwork",
+    "magick",
+    "see",
+    "lmtest",
+    "car",
+    "glmmTMB",
+    "performance",
+    "multcomp",
+    "effectsize",
+    "AICcmodavg",
+    "bayesplot",
+    "posterior",
+    "pwr",
+    "ICC",
+    "DescTools",
+    "cvTools",
+    "epiDisplay",
+    "Rmpfr",
+    "smatr",
+    "factoextra",
+    "mvtnorm",
+    "ape",
+    "phytools",
+    "sf",
+    "rnaturalearth",
+    "rnaturalearthdata",
+    "knitr",
+    "gt",
+    "sjPlot",
+    "pander",
+    "tictoc",
+    "furrr",
+    "paletteer"
+  ))
+
+  pak::pkg_install("stan-dev/cmdstanr")
+  pak::pkg_install("rmcelreath/rethinking")
+
+  if (!require("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager")
+  }
+
+  BiocManager::install("qvalue")
+  BiocManager::install("ggtree")
+}
+
+
+qmds <- list.files(
+  pattern = "^[0-9]*-[1-6].*qmd$",
+  path = "Lectures/",
+  full.names = TRUE
+)
 
 for (qq in qmds) {
   message("Processing ", qq)
@@ -13,9 +86,11 @@ for (qq in qmds) {
 
 ## Problem sets
 
-qmds <- list.files(pattern = "^PS_[0-9]*[_Key]*.qmd$",
-                   path = "Problem_Sets",
-                   full.names = TRUE)
+qmds <- list.files(
+  pattern = "^PS_[0-9]*[_Key]*.qmd$",
+  path = "Problem_Sets",
+  full.names = TRUE
+)
 
 for (qq in qmds) {
   message("Processing ", qq)
