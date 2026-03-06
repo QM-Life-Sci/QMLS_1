6ed48f2171c32935f51869355b5ff41a68ac088c
Kevin Middleton <middletonk@missouri.edu>
2026-03-06 10:58:05 -0600

    Update 06-06 In Class Demo.R

--- Lectures/06-06 In Class Demo.R
+++ Lectures/06-06 In Class Demo.R
@@ -7,6 +7,7 @@ b <- 5
 
 dd <- tibble(x = runif(n = 100, min = -10, max = 10),
              y = b * x + rnorm(n = 100, mean = 10, sd = 3))
+dd <- rbind(dd, c(-7, 15))
 
 ggplot(dd, aes(x, y)) +
   geom_point() +
