180db597a4eb7dbc8c5ccedda34b66acaf974c8d
Kevin Middleton <middletonk@missouri.edu>
2026-03-06 10:57:45 -0600

    Update Unit 6

---
+++ Lectures/06-06 In Class Demo.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(performance)
+
+set.seed(34747322)
+
+b <- 5
+
+dd <- tibble(x = runif(n = 100, min = -10, max = 10),
+             y = b * x + rnorm(n = 100, mean = 10, sd = 3))
+
+ggplot(dd, aes(x, y)) +
+  geom_point() +
+  geom_smooth(formula = y ~ x, method = "lm", se = FALSE)
+
+fm <- lm(y ~x, dd)
+check_model(fm)
